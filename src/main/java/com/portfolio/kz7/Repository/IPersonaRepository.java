9ee5cf2c940fcc6e98a768bd4c64756a54dd6793
Correa Edgar Gabriel <keizaby7@gmail.com>
2022-10-31 23:27:02 -0300

    Primero

---
+++ src/main/java/com/portfolio/kz7/Repository/IPersonaRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.kz7.Repository;
+
+import com.portfolio.kz7.Entity.Persona;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Integer>{
+
+    public Optional<Persona> findByNombre(String nombre);
+
+    public boolean existsByNombre(String nombre);
+}
